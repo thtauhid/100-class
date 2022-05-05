8012024440c0564163e58f9dd14ec49380a9d7d5
Tasnimul H. Tauhid <thtauhid.71@gmail.com>
2022-05-05 22:55:09 +0530

    Minor style update of Class Module

--- src/comps/ClassCard.js
+++ src/comps/ClassCard.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import '../style/classes.css'
+
 
 function ClassCard({data}) {
   return (
     <>
-        <div className="card border-secondary mb-3" style={{maxWidth: '20rem'}}>
+        <div className="card border-secondary pointer mb-3" style={{maxWidth: '20rem'}}>
             <div className="card-header d-flex justify-content-between align-items-center"> 
                 <h4 className="card-title"><Link to={data.link}>{data.title}</Link></h4> 
                 <img src={data.avatar} style={{borderRadius: 50}} height="50"  />
