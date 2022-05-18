a86c3a3b783633d6287c7384f498e1d643f55019
Tasnimul H. Tauhid <thtauhid.71@gmail.com>
2022-05-19 05:13:18 +0600

    Adding Icon to banner

--- src/comps/Banner.js
+++ src/comps/Banner.js
@@ -1,9 +1,15 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFolder } from '@fortawesome/free-solid-svg-icons'
+
 function Banner({bg}) {
   return (
     <>
-      <div className="card mb-3 mt-3 p-5" style={{background: bg}}>
-        <h1 className='text-white'>DBMS</h1>
-        <h4 className="text-white">01IT0402</h4>
+      <div className="card mb-3 mt-3 p-5 banner-container" style={{background: bg}}>
+        <FontAwesomeIcon className='banner-icon' icon={faFolder} />
+        <div className="banner-details">
+          <h1 className='text-white'>DBMS</h1>
+          <h4 className="text-white">01IT0402</h4>
+        </div>
       </div>
     </>
   )
