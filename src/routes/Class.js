f8e1662bdf5391e04bd048f92be59a5fa287407d
Tasnimul H. Tauhid <thtauhid.71@gmail.com>
2022-05-19 18:54:37 +0600

    Upcoming section migration

--- src/routes/Class.js
+++ src/routes/Class.js
@@ -1,8 +1,8 @@
 import Nav from '../Nav'
 import Banner from '../comps/Banner'
-import Upcoming from '../comps/Upcoming'
 import Content from '../comps/Content'
-
+import upcoming from '../config/upcoming'
+import List from '../comps/List'
 function Class() {
   return (
     <>
@@ -13,7 +13,10 @@ function Class() {
 
             
             <aside className='col-md-3'>
-                <Upcoming />
+              <div className="upcoming-container">
+                  <h2 className='mb-3'>Upcoming</h2>
+                  <List data={upcoming} />
+              </div>
             </aside>
             <main className='col-md-9'>
                 {/* Posting Area */}
