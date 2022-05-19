abf64ab9d58eec08e5addbcefffa4735974931b4
Tasnimul H. Tauhid <thtauhid.71@gmail.com>
2022-05-19 18:45:13 +0600

    Updated list style

--- src/comps/List.js
+++ src/comps/List.js
@@ -2,13 +2,22 @@ import { Link } from 'react-router-dom'
 
 function List({data}) {
   return (
-    <div className='list-group'>
+    <>
+      <ul className="list-container">
         {
             data.map(item => {
-                return <Link className='list-group-item list-group-item-action' to={item.link}> {item.title} </Link>
+                return <li className='list-item'><Link className='' to={item.link}> {item.title} </Link></li>
             })
         }
-    </div>
+      </ul>
+    </>
+    // <div className='list-group'>
+    //     {
+    //         data.map(item => {
+    //             return <Link className='list-group-item list-group-item-action' to={item.link}> {item.title} </Link>
+    //         })
+    //     }
+    // </div>
   )
 }
 
