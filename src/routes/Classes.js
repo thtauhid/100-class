e7a60c136c881327d3511cfea15c2c35e23ff30c
Tasnimul H. Tauhid <thtauhid.71@gmail.com>
2022-05-20 22:02:28 +0600

    Pulling data from firebase

--- src/routes/Classes.js
+++ src/routes/Classes.js
@@ -2,24 +2,28 @@ import React, {useState, useEffect} from 'react'
 import ClassCard from '../comps/ClassCard'
 import Nav from '../Nav'
 import { db } from '../config/firebase';
-import { collection, getDocs } from "firebase/firestore";
-
+import { collection, getDocs, doc, getDoc } from "firebase/firestore";
 function Classes() {
 
   const [classes, setClasses] = useState([]);
   
-  const classesCollectionRef = collection(db, "classes");
-
   useEffect(() => {
-    const getUsers = async () => {
-      const data = await getDocs(classesCollectionRef);
-      setClasses(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
+
+    const getClasses = async () => {
+      const classesCollectionRef = collection(db, "classes");
+      const classData = await getDocs(classesCollectionRef)
+
+      const data = classData.docs.map(item => {
+        return {...item.data(), id: item.id}
+      }) 
+
+      setClasses(data)
+      
     }
 
-    getUsers();
-  }, [])
-  
+    getClasses();
 
+  }, [])
   
   return (
     <>
